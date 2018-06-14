f9cf3e50e35e002e8c44aaaeb2945303c8eba045
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-14 14:22:10 +0800

    add hci gatt debug

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/controller/CMakeFiles/subsys__bluetooth__controller.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/controller/CMakeFiles/subsys__bluetooth__controller.dir/DependInfo.cmake
@@ -29,7 +29,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-152-gad43c3c"
+  "BUILD_VERSION=v1.12.0-153-ga9c4ab8"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
