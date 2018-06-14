fb66b6d6b309f1cc7c05c260d3085371718259b3
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-14 14:02:31 +0800

    1.add system debug

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/controller/CMakeFiles/subsys__bluetooth__controller.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/controller/CMakeFiles/subsys__bluetooth__controller.dir/DependInfo.cmake
@@ -29,7 +29,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-147-g579a62f"
+  "BUILD_VERSION=v1.12.0-148-g88f4f89"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
