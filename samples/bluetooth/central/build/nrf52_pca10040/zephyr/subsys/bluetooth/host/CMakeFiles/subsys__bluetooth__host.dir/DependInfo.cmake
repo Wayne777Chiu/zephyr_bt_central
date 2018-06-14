4f46e6013ce61173001a31a4f1d8cedcf17efe02
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-14 14:25:28 +0800

    disable gatt debug

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/host/CMakeFiles/subsys__bluetooth__host.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/host/CMakeFiles/subsys__bluetooth__host.dir/DependInfo.cmake
@@ -16,7 +16,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-153-ga9c4ab8"
+  "BUILD_VERSION=v1.12.0-154-gf9cf3e5"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
