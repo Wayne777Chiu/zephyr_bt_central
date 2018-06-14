393ee0aaf0ec0bdefb079228f177cc423d2f8e8f
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-14 18:11:16 +0800

    Enable hci driver debug

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/CMakeFiles/zephyr.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/CMakeFiles/zephyr.dir/DependInfo.cmake
@@ -30,7 +30,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-158-geb726d8"
+  "BUILD_VERSION=v1.12.0-159-gbf12b3d"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
