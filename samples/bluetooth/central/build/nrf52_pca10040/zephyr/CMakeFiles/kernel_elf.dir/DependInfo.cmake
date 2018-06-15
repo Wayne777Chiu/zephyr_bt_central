4d43853f112268f2d65eaecbfb97b04e10c6bc6a
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-15 13:58:25 +0800

    update

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/CMakeFiles/kernel_elf.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/CMakeFiles/kernel_elf.dir/DependInfo.cmake
@@ -11,7 +11,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-170-g19c8703"
+  "BUILD_VERSION=v1.12.0-171-ga95645d"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/arch/arm/core/cortex_m/CMakeFiles/arch__arm__core__cortex_m.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/arch/arm/core/cortex_m/CMakeFiles/arch__arm__core__cortex_m.dir/DependInfo.cmake
@@ -13,7 +13,7 @@ set(CMAKE_ASM_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_ASM
-  "BUILD_VERSION=v1.12.0-170-g19c8703"
+  "BUILD_VERSION=v1.12.0-171-ga95645d"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
@@ -54,7 +54,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-170-g19c8703"
+  "BUILD_VERSION=v1.12.0-171-ga95645d"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
