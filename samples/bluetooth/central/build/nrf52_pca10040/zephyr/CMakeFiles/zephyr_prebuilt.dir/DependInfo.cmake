fbac96fffc664956f9d661902c63b431d2cc119d
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-15 15:45:55 +0800

    update debug

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/CMakeFiles/zephyr_prebuilt.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/CMakeFiles/zephyr_prebuilt.dir/DependInfo.cmake
@@ -10,7 +10,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-186-gcec15db"
+  "BUILD_VERSION=v1.12.0-187-ge44c4ea"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
