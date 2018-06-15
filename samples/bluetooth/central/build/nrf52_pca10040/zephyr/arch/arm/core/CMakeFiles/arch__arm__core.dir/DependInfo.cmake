85939a2054cf9a0e077a577575b96fee79a32ecc
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-15 14:41:10 +0800

    update

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/arch/arm/core/CMakeFiles/arch__arm__core.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/arch/arm/core/CMakeFiles/arch__arm__core.dir/DependInfo.cmake
@@ -15,7 +15,7 @@ set(CMAKE_ASM_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_ASM
-  "BUILD_VERSION=v1.12.0-179-g538d2e6"
+  "BUILD_VERSION=v1.12.0-180-gc77a1fb"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
@@ -60,7 +60,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-179-g538d2e6"
+  "BUILD_VERSION=v1.12.0-180-gc77a1fb"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
