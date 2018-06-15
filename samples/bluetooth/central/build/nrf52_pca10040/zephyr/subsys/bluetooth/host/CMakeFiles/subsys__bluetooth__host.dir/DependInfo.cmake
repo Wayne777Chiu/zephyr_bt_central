d509b65a397a5558a74eb8a76c0cf4c679111cd0
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-15 14:58:24 +0800

    update

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/host/CMakeFiles/subsys__bluetooth__host.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/host/CMakeFiles/subsys__bluetooth__host.dir/DependInfo.cmake
@@ -16,7 +16,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-181-g85939a2"
+  "BUILD_VERSION=v1.12.0-182-g806f93c"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
