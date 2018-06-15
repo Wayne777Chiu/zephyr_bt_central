4ac9f2817fc0f2dd639271da7ff4839bd197cba4
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-15 16:32:13 +0800

    update disable hci core

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/common/CMakeFiles/subsys__bluetooth__common.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/subsys/bluetooth/common/CMakeFiles/subsys__bluetooth__common.dir/DependInfo.cmake
@@ -12,7 +12,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-195-g55f9022"
+  "BUILD_VERSION=v1.12.0-196-g1055d3f"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
