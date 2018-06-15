79c7f7b22f2d98d38809c862902403d99b900c39
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-15 13:10:31 +0800

    hci core debug

--- samples/bluetooth/central/build/nrf52_pca10040/zephyr/lib/libc/minimal/CMakeFiles/lib__libc__minimal.dir/DependInfo.cmake
+++ samples/bluetooth/central/build/nrf52_pca10040/zephyr/lib/libc/minimal/CMakeFiles/lib__libc__minimal.dir/DependInfo.cmake
@@ -19,7 +19,7 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
-  "BUILD_VERSION=v1.12.0-161-g61f12fa"
+  "BUILD_VERSION=v1.12.0-162-gb15e8fb"
   "KERNEL"
   "NRF52832_XXAA"
   "_FORTIFY_SOURCE=2"
